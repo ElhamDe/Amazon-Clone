ebe595d8e2d55a2f4ec020d4e1dc1990a08d4039
Elham Dermaku <dermaku12@gmail.com>
2023-06-02 22:57:50 +0200

    before clicking paths

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { BrowserRouter, Routes, Route  } from "react-router-dom";
-import { HomePage } from "./components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomePage, NavBar, Checkout, SearchResults, ProductPage } from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
+        <NavBar />
         <Routes>
             <Route exact path="/" element={<HomePage />}/>
+            <Route path="/search" element={<SearchResults />}/>
+            <Route path="/product" element={<ProductPage />}/>
+            <Route path="/checkout" element={<Checkout />}/>
         </Routes>
     </BrowserRouter>
   )
