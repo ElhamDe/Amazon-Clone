a2891b3e908b046da9a8d76d92d12e91789ebb22
Elham Dermaku <dermaku12@gmail.com>
2023-06-03 19:38:16 +0200

    finish

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -20,10 +20,36 @@ export const cartSlice = createSlice({
             state.productsNumber = state.productsNumber + parseInt(action.payload.quantity);
         },
         removeFromCart: (state, action) => {
-
-        }
-    }
-});
-
-export const { addToCart, removeFromCart } = cartSlice.actions;
+            // find the product removing the array
+            const productToRemove = state.products.find((product) => product.id === action.payload);
+            // remove the quantity from product number
+            state.productsNumber = state.productsNumber - productToRemove.quantity;
+            // find index of the prodct removing
+            const index = state.products.findIndex((product) => product.id === action.payload);
+            // remove from the array
+            state.products.splice(index, 1);
+        },
+        incrementInCart: (state, action) => {
+            const itemInc = state.products.find((item) => item.id === action.payload);
+            if (itemInc.quantity >= 1) {
+              itemInc.quantity = itemInc.quantity + 1;
+            }
+            state.productsNumber = state.productsNumber + 1;
+          },
+          decrementInCart: (state, action) => {
+            const itemDec = state.products.find((item) => item.id === action.payload);
+            if (itemDec.quantity === 1) {
+              const index = state.products.findIndex(
+                (item) => item.id === action.payload
+              );
+              state.products.splice(index, 1);
+            } else {
+              itemDec.quantity--;
+            }
+            state.productsNumber = state.productsNumber - 1;
+          },
+        },
+      });
+      
+export const { addToCart, removeFromCart, incrementInCart, decrementInCart } = cartSlice.actions;
 export default cartSlice.reducer;
